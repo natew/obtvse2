1fab4e98039e23350d40484dfa6bea53b68ef9d3
andy rose <thatandyrose@gmail.com>
2013-10-02 21:48:43 +1000

    trying out erb in new relic yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :newrelic_key
 
   validates_confirmation_of :password
   validates_presence_of :password, on: :create
