52cecef16a36d04df83d5616bde1290bb922635d
Laura <lauradhamilton@gmail.com>
2013-12-19 15:46:36 -0600

    Fixing spelling mistake in comment

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,7 +2,7 @@ class SitemapController < ApplicationController
   def index
     @static_pages = [root_url]
     @users = User.all
-    # Desplay all the posts in reverse chronological order
+    # Display all the posts in reverse chronological order
     @posts = Post.all.sort.reverse
     respond_to do |format|
       format.xml
