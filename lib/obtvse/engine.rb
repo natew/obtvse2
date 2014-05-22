26e55ef2dcb86accdb15f75b00dc90282403a9e8
Nate Berkopec <nate.berkopec@gmail.com>
2014-05-22 11:51:02 -0400

    Append migrations to app root

--- lib/obtvse/engine.rb
+++ lib/obtvse/engine.rb
@@ -19,5 +19,13 @@ module Obtvse
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
